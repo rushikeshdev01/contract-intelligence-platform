d8ff27c3c05cc80adcc37f86650e926ace9d7381
rushikeshdev01 <rushikeshdev75@gmail.com>
2026-06-14 17:42:15 +0530

    add pypdf feature

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,18 @@
 import streamlit as st
 import os
+from pypdf import PdfReader
+
+
+def extract_pdf_text(pdf_path):
+    reader = PdfReader(pdf_path)
+
+    text = ""
+
+    for page in reader.pages:
+        text += page.extract_text() + "\n"
+
+    return text
+
 
 st.title("Multi-Agent Contract Intelligence Platform")
 

8d29a457b385019bebc53ead8180c9e35de0b19f
rushikeshdev01 <rushikeshdev75@gmail.com>
2026-06-16 18:35:32 +0530

    add pdf extraction

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,6 @@
 import streamlit as st
 import os
-from pypdf import PdfReader
-
-
-def extract_pdf_text(pdf_path):
-    reader = PdfReader(pdf_path)
-
-    text = ""
-
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
-
-    return text
+from services.pdf_service import extract_pdf_text
 
 
 st.title("Multi-Agent Contract Intelligence Platform")
